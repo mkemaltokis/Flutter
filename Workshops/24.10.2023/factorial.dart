5d4315c45046e3e1afec11dde3fd8849a06fa55e
Muhammed Kemal Tokis <148941379+mkemaltokis@users.noreply.github.com>
2023-10-26 11:18:10 +0300

    Factorial and Prime Number Calculator
    Tobeto workshop

---
+++ Workshops/24.10.2023/factorial.dart
@@ -0,0 +1,9 @@
+void main(){
+    int n = 5; //Enter the N value you want to calculate.
+    int sonuc = 1;
+    for (var i = 1; i <= n; i++ ){
+         sonuc*=i;
+        
+        }
+print("${n} Factorial = ${sonuc}");
+}
---
+++ Workshops/24.10.2023/primeNumber.dart
@@ -0,0 +1,29 @@
+void main(){
+    
+    int n=100 ;  //Enter the N value you want to calculate.
+    for (var i = 1; i < n; i++) {
+      if(i==2){
+        print(2);
+        }
+        else if(i==3){
+        print(3);
+        }
+        else if(i==5){
+        print(5);
+        }
+        else if(i==7){
+        print(7);
+        }
+        else if(i%2==0){
+        }
+        else if(i%3==0){
+        }
+        else if(i%5==0){
+        }
+        else if(i%7==0){
+        }
+        else{
+        print(i);
+        }
+ }
+}
