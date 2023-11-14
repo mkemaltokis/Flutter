e92e3bb35fd9e7fad5393c4148352be445494e22
Muhammed Kemal Tokis <148941379+mkemaltokis@users.noreply.github.com>
2023-11-14 08:31:42 +0300

    expenseApp

---
+++ Workshops/9.11.2023/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/expenses_page.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: ExpensesPage(),
+      ),
+    ),
+  );
+}
