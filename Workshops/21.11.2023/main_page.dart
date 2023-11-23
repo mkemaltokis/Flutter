5bac54e47b6a12f4514b6d80ceb437a990fc2429
Muhammed Kemal Tokis <148941379+mkemaltokis@users.noreply.github.com>
2023-11-23 19:15:05 +0300

    Create main_page.dart
    Snackbar

---
+++ Workshops/21.11.2023/main_page.dart
@@ -0,0 +1,69 @@
+import 'package:expenseapp/models/expense.dart';
+import 'package:expenseapp/pages/expenses_page.dart';
+import 'package:expenseapp/widgets/new_expense.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List<Expense> expenses = [
+    Expense(
+        name: "Yemek",
+        price: 500.529,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        name: "Udemy Kursu",
+        price: 200,
+        date: DateTime.now(),
+        category: Category.work),
+  ];
+
+  addExpense(Expense expense) {
+    setState(() {
+      expenses.add(expense);
+    });
+  }
+
+  removeExpense(Expense expense) {
+    setState(() {
+      expenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: const Text("Harcama silindi"),
+      action: SnackBarAction(
+          label: 'Geri Al',
+          onPressed: () {
+            setState(() {
+              expenses.add(expense);
+            });
+          }),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Expense App"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (ctx) {
+                      return NewExpense(addExpense);
+                    });
+              },
+              icon: const Icon(Icons.add))
+        ],
+      ),
+      body: ExpensesPage(expenses, removeExpense),
+    );
+  }
+}
